2fb3feae9c38db33c8ab726a60a01caa21293ffa
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-03-09 13:52:01 -0800

    greyboxframe

---
+++ app.js
@@ -0,0 +1,76 @@
+//Quiz questions
+var data = [
+     {
+          number:1
+          question:"'In Nausicaa of the Valley of the Wind,' what threatens the existence of humans?"
+          answers:[ 'Nausicaa','Erosion','The toxic jungle','The dragons' ]
+          correctAnswer:2
+     },
+     {
+          number:2
+          question:"How do Mei, Satsuki and their father make the sootballs leave in 'My Neighbor Totoro'?"
+          answers:[ 'Crying','Screaming','Laughing','Jumping']
+          correctAnswer:2
+     },
+     {
+          number:3
+          question:"What animal do Chirhiro's parents both get turned into in 'Spirited Away'?"
+          answers:['Pig','Hedgehog','Bunny','Monkey' ]
+          correctAnswer:0
+     },
+     {
+          number:4
+          question:"What is Kiki's cat named in 'Kiki's Delivery Service'?"
+          answers:[ 'Joe','Gigo','Jay','Jiji']
+          correctAnswer:3
+     },
+     {
+          number:5
+          question:"What natural disaster happened during 'Ponyo on the Cliff by the Sea'?"
+          answers:['Earthquake','Tornado','Tsunami','Mudslide']
+          correctAnswer:2
+     },
+     {
+          number:6
+          question:"What were the pirates trying to steal from Sheeta in 'Laputa: Castle in the Sky'?"
+          answers:['Necklace','Bracelet','Earings','Ring']
+          correctAnswer:0
+     },
+     {
+          number:7
+          question:"Where was Ashitaka's infection spreading from in 'Princess Mononoke'?"
+          answers:['His leg','His head','His chest','His arm']
+          correctAnswer:3
+     },
+     {
+          number:8
+          question:"What color did Nausicaa's dress change in 'Nausicaa of the Valley of the Wind'?"
+          answers:['Pink','Orange','Blue','Purple']
+          correctAnswer:2
+     },
+     {
+          number:9
+          question:"What does Arietty use to hold her hair back in 'The Secret World of Arietty'?"
+          answers:['Rubber Band','Clothespin','Ring','Tape']
+          correctAnswer:1
+     },
+     {
+          number:10
+          question:"What is the flaming creature's name in 'Howl's Moving Castle?'"
+          answers:[ 'Crucifer','Calcifer','Calculator','Calcium' ]
+          correctAnswer:1
+     },
+     {
+          number:11
+          question:"In The Wind Rises, why can't Jiro become a pilot?"
+          answers:[ 'He is blind','He doesn\'t know how to drive one','He doesn\'t have enough money to go to college','He is nearsighted' ]
+          correctAnswer:3
+     },
+     {
+          number:12
+          question:'Which character from Tales of Earthsea says, "Life without death is not life."'
+          answers:[ 'Haitaka','Teru','Sparrowhawk','Hare' ]
+          correctAnswer:0
+     }
+
+]
